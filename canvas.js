7d4a25531a72e406e942d476dab75030169e5aa5
Monroe <me_menglu@163.com>
2021-03-30 20:16:37 +0800

    增加移动端touch事件

--- canvas.js
+++ canvas.js
@@ -5,20 +5,35 @@ let ctx = canvas.getContext("2d");
 let painting = false;
 ctx.fillStyle = "black";
 ctx.strokeStyle = 'none';
+
+let isTouchDevice = "ontouchstart" in
+    document.documentElement;
+
+if (isTouchDevice) {
+    canvas.ontouchmove = (e) => {
+        let x = e.touches[0].clientX;
+        let y = e.touches[0].clientY;
+
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, 2 * Math.PI);
+   ctx.stroke();
+     ctx.fill();
+
+    }
+}else{
+
 canvas.onmousedown = () => {
     painting = true;
 };
 canvas.onmousemove = (e) => {
     if (painting === true) {
         ctx.beginPath();
-        
         ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.fill();
-    } else {
-        console.log('什么都不做')
-    }
+    } 
 };
    canvas.onmouseup = () => {
             painting = false;
         }
+}
